35069c8031af5ad0b27db67760d3d017acf1f032
schengsi93 <54499069+schengsi93@users.noreply.github.com>
2020-02-08 00:19:18 +0800

    Update PurchaseOrder.cs

--- AD_Team10/Models/PurchaseOrder.cs
+++ AD_Team10/Models/PurchaseOrder.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-<<<<<<< HEAD
 using System.ComponentModel.DataAnnotations;
-=======
->>>>>>> 10578a09f5d6cdf27db9a235fc7f8c6c6b60fd2b
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
@@ -16,7 +13,6 @@ namespace AD_Team10.Models
     }
     public class PurchaseOrder
     {
-<<<<<<< HEAD
         [Display(Name = "Purchase Order ID")]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int PurchaseOrderID{ get; set; }
@@ -28,14 +24,6 @@ namespace AD_Team10.Models
         [Display(Name = "Completed Date")]
         public DateTime CompletedDate { get; set; }
         [Display(Name = "Status")]
-=======
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int PurchaseOrderID{ get; set; }
-        [ForeignKey("Supplier")]
-        public int SupplierID { get; set; }
-        public DateTime OrderDate { get; set; }
-       
->>>>>>> 10578a09f5d6cdf27db9a235fc7f8c6c6b60fd2b
         public OrderStatus? OrderStatus { get; set; }
 
         public virtual Supplier Supplier { get; set; }
