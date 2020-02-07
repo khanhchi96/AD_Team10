7bb63cc10a2ec2dd10381fe07a89136b57ebb5ce
schengsi93 <54499069+schengsi93@users.noreply.github.com>
2020-02-08 00:31:47 +0800

    Update RepresentativeController.cs

--- AD_Team10/Controllers/Department/RepresentativeController.cs
+++ AD_Team10/Controllers/Department/RepresentativeController.cs
@@ -8,12 +8,6 @@ using AD_Team10.Authentication;
 namespace AD_Team10.Controllers.Department
 {
     [CustomAuthorize(Roles = "REPRESENTATIVE")]
-=======
-
-namespace AD_Team10.Controllers.Department
-{
-    [Authorize(Roles = "REPRESENTATIVE")]
->>>>>>> 10578a09f5d6cdf27db9a235fc7f8c6c6b60fd2b
     public class RepresentativeController : Controller
     {
         public ActionResult Index()
